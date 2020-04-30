02cb5fb08b4606087f5a5e10e2356e55ac548266
Dylan Barber <dyleighba@gmail.com>
2020-05-01 00:42:27 +0800

    Replaced NonBlockingStdIn booleans with threading objects
    * __running is now of type threading.Event and called __kill_flag 
    * __paused is now of type threading.Lock and called __lines_lock

--- __init__.py
+++ __init__.py
@@ -13,61 +13,56 @@ class NonBlockingStdIn:
 
     # This sets the max amount of lines to be stored in lines[] at once
     def __init__(self, line_limit: int = 0) -> None:
-
         # Attributes are private to prevent severe turmoil
         self.__line_limit = abs(line_limit) or float('inf')
-        self.__running = True
-        self.__paused = False
+        self.__kill_flag = threading.Event()
         self.__lines = []
+        self.__lines_lock = threading.Lock()
         self.__thread = threading.Thread(target=self.__input_collector)
-        self.__thread.daemon = True
         self.__thread.start()
 
     # Private process used by thread - collects input continuously
     def __input_collector(self) -> None:
-        while self.__running:
-            if len(self.__lines) < self.__line_limit:
+        while True:
+            if self.__kill_flag.is_set():
+                return
+            elif len(self.__lines) < self.__line_limit:
                 try:
                     line = input()
                 except EOFError:
                     continue
-                while self.__paused:
-                    pass
+                self.__lines_lock.acquire()
                 self.__lines.append(line)
+                self.__lines_lock.release()
 
     # Non-blocking input method, pulls line from buffer, None when empty
     def input(self) -> Union[str, None]:
-        self.__paused = True
-
-        if len(self.__lines) == 0:
-            line = None
-        else:
+        self.__lines_lock.acquire()
+        line = None
+        if len(self.__lines) > 0:
             line = self.__lines.pop(0)
-
-        self.__paused = False
+        self.__lines_lock.release()
         return line
 
     # Returns number of lines buffered
     def num_available_lines(self) -> int:
-        self.__paused = True
+        self.__lines_lock.acquire()
         amount = len(self.__lines)
-        self.__paused = False
+        self.__lines_lock.release()
         return amount
 
     # Returns all buffered lines and clears buffer
     def get_all_lines(self) -> List[str]:
-        self.__paused = True
-
+        self.__lines_lock.acquire()
         lines = self.__lines
         self.__lines = []
-
-        self.__paused = False
+        self.__lines_lock.release()
         return lines
 
     # Necessary to shutdown thread properly and restore input() method
     # Sadly requires user to place line into stdin to free-up the blocking input()
     def kill(self):
-        self.__running = False
+        self.__kill_flag.set()
         print('Please hit [ENTER]')
         self.__thread.join()
 
